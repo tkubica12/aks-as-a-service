61d29a1a1ed915c259f9cdca200f145e580a3731
tkubica12 <tkubica12@gmail.com>
2023-10-23 15:48:08 +0200

    remove name

--- modules/aks-system/monitoring.tf
+++ modules/aks-system/monitoring.tf
@@ -10,7 +10,6 @@ resource "azurerm_monitor_data_collection_rule_association" "dcra" {
 // Association of AKS cluster with DCE for Prometheus collection
 resource "azurerm_monitor_data_collection_rule_association" "dcea" {
   count                       = var.prometheus_dce_id != "" ? 1 : 0
-  name                        = var.name
   target_resource_id          = azurerm_kubernetes_cluster.main.id
   data_collection_endpoint_id = var.prometheus_dce_id
   description                 = "Association of data collection rule. Deleting this association will break the data collection for this AKS Cluster."
